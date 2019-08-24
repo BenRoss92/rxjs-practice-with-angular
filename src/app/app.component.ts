a21cdfbb76cb0e67782679dc3db594e36f99cbef
Ben Ross <benjamin.ross@youview.com>
2019-08-24 17:26:45 +0100

    Combine values from two different observable streams

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 /**
  * Challenge:
  *
- * Create an observable stream that emits values incrementally every second, starting from 0.
+ * Create an observable stream that emits numbers incrementally every second, starting from 0.
  * 
- * Then take the first 5 emitted values only.
+ * Create an observable stream that emits the letters a-e.
+ * 
+ * Then take the first 5 emitted numbers only.
  *
- * Then create another observable stream from this previous stream that emits those values multiplied by 10.
- * 
- * Then filter those values by those only greater than 20.
+ * Then each second, combine the number value with each letter
+ * - have the letter observable emit all letters every second
+ * - combine the two observable streams - that way we will emit multiple letters every time a number is emitted.
  * 
  * Log the final emitted values to the console.
  *
@@ -15,15 +17,16 @@
 
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 /**
  * import 'interval' and `add` it to Observable's prototype
  * N.B. use the `/add/` folder to only pull in that method to reduce the overall bundle size
  * see https://www.learnrxjs.io/concepts/operator-imports.html
  */
 import 'rxjs/add/observable/interval';
-import 'rxjs/add/operator/take';
+import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/take';
 
 @Component({
   selector: 'app-root',
@@ -34,33 +37,30 @@ export class AppComponent {
   title = 'app';
 
   numbers$;
+  letters$;
 
   ngOnInit() {
     // Creates an observable stream that emits values incrementally every second, starting from 0
     this.numbers$ = Observable.interval(1000);
+    // Creates an observable stream that emits the letters a-e in sequence
+    this.letters$ = Observable.of('a', 'b', 'c', 'd', 'e');
 
-    // creates a new observable stream, taking the first 5 emitted values only.
-    this.numbers$
-      .take(5)
-      // creates a new observable stream that multiplies each emitted value by 10.
-      .map(number => number * 10)
-      // filter the emitted values by those greater than 20, then emit only these values in a new observable stream
-      .filter(number => number > 20)
-
-      // subscribe to the `numbers$` observable for when it will emit values
-      .subscribe(
-        // onNext handler - each time a value is emitted to the stream, log it to the console
-        number => console.log(number),
-        // onError handler - if an error is emitted, log it to the console
-        err => console.error(err),
-        // onCompletion handler - when stream has completed, log out a completion message to the console
-        () => console.log('stream has completed!')
-      );
-
+    // Creates and returns a new observable that merges the emitted values from the letters and numbers observables
+    // Merges the emitted values from the letters observable into the numbers observable to emit all letters every second
+    this.letters$.mergeMap((letter) => {
+      // N.B. Callback function needs to return an observable for mergeMap
+      return this.numbers$
+        .take(5)
+        .map(number => number + letter);
+    })
+    // immediately subscribe to the new returned observable
+    // N.B. Make sure you only subscribe to the resulting values after they have been through the operators
+    .subscribe(value => console.log(value));
   }
 
   ngOnDestroy() {
-    // unsubscribe from the `numbers$` stream when the component is destroyed to avoid a memory leak
+    // unsubscribe from the `numbers$` and `letters$` streams when the component is destroyed to avoid a memory leak
     this.numbers$.unsubscribe();
+    this.letters$.unsubscribe();
   }
 }
